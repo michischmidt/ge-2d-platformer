601d3f4dd90a06a3d3dc1b20243bfc6b155c9472
michischmidt <schmidt.m.dev@gmail.com>
2022-03-04 16:34:48 +0100

    updated sword hit and added enemy sprites

--- Assets/Scripts/Player/PlayerCombat.cs
+++ Assets/Scripts/Player/PlayerCombat.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class PlayerCombat : MonoBehaviour {
 
     public Animator anim;
+    public Transform swordAttackPoint;
+    public float attackRange = 0.5f;
+    public LayerMask enemyLayers;
 
     // Start is called before the first frame update
     void Start() {
@@ -12,19 +15,34 @@ public class PlayerCombat : MonoBehaviour {
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
         if (Input.GetKeyDown(KeyCode.Space)) {
             Attack();
         }
     }
 
-    void Attack() {
+    public void Attack() {
         // Play attack animation
         anim.SetTrigger("SwordAttack");
 
         // Detect enemy in range
+        // Creates cirlce and collects all objects that are hit by it
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(swordAttackPoint.position, attackRange, enemyLayers);
+
 
         // Deal DMG to enemy
+        foreach(Collider2D enemy in hitEnemies) {
+            Debug.Log("Hit " + enemy.name);
+        }
+    }
+
+    // Development function
+    void OnDrawGizmosSelected() {
+        if (swordAttackPoint == null) {
+            return;
+        }
+
+        Gizmos.DrawWireSphere(swordAttackPoint.position, attackRange);
     }
 }
+
