7093ab96e577274e4b58e61c097a944699785b35
michischmidt <schmidt.m.dev@gmail.com>
2022-03-11 18:44:34 +0100

    added hitbox script

---
+++ Assets/Scripts/Enemies/HitBoxCheck.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitBoxCheck : MonoBehaviour {
+
+    public PlayerHealth ph;
+    public int dmg = 1;
+
+    private void OnTriggerEnter2D(Collider2D collider) {
+        if (collider.gameObject.CompareTag("Player")) {
+            ph.DamagePlayer(dmg);
+        }
+    }
+}
--- Assets/Scripts/Enemies/TriggerAreaCheck.cs
+++ Assets/Scripts/Enemies/TriggerAreaCheck.cs
@@ -18,5 +18,4 @@ public class TriggerAreaCheck : MonoBehaviour {
             enemyParent.hotZone.SetActive(true);
         }
     }
-
 }
