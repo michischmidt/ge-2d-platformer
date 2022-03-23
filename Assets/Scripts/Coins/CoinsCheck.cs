ac82c5ca598f8d8a6668bfd0deb3106fb2b4f892
michischmidt <schmidt.m.dev@gmail.com>
2022-03-23 22:37:20 +0100

    added coins

---
+++ Assets/Scripts/Coins/CoinsCheck.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CoinsCheck : MonoBehaviour {
+    public Text coinsText;
+    [SerializeField] private AudioClip coinSound;
+    int coinsCount = 0;
+
+    private void OnTriggerEnter2D(Collider2D collider) {
+        if (collider.gameObject.CompareTag("Player")) {
+            SoundManager.instance.PlaySound(coinSound);
+            coinsCount += 1;
+            coinsText.text = coinsCount.ToString();
+            Destroy(gameObject);
+        }
+    }
+}
